2c0fb8a25ca9415d54e7043c470a8d4d9e16c607
Kevin Stenerson <kevin@rapiditynetworks.com>
2017-09-10 22:39:39 -0600

    Fix host URL

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,6 @@
 use error::Error;
 use hyper;
 use hyper::client::RequestBuilder;
-use hyper::header::{Authorization, Basic, ContentType, Headers};
 use hyper::net::HttpsConnector;
 use serde;
 use serde_json as json;
@@ -9,25 +8,21 @@ use std::io::Read;
 
 pub struct Client {
     client: hyper::Client,
-    secret_key: String,
 }
 
 impl Client {
     fn url(path: &str) -> String {
-        format!("https://api.stripe.com/v1/{}", &path[1..])
+        format!("https://www.googleapis.com/oauth2/v3/{}", &path[1..])
     }
 
     #[cfg(feature = "with-rustls")]
-    pub fn new<Str: Into<String>>(secret_key: Str) -> Client {
+    pub fn new() -> Client {
         use hyper_rustls::TlsClient;
 
         let tls = TlsClient::new();
         let connector = HttpsConnector::new(tls);
         let client = hyper::Client::with_connector(connector);
-        Client {
-            client: client,
-            secret_key: secret_key.into(),
-        }
+        Client{client}
     }
 
     #[cfg(feature = "with-openssl")]
@@ -37,10 +32,7 @@ impl Client {
         let tls = OpensslClient::new().unwrap();
         let connector = HttpsConnector::new(tls);
         let client = hyper::Client::with_connector(connector);
-        Client {
-            client: client,
-            secret_key: secret_key.into(),
-        }
+        Client{client}
     }
 
     pub fn get<T: serde::de::DeserializeOwned>(&self, path: &str) -> Result<T, Error> {
