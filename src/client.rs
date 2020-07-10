4bb42023f32fb24b37d173491ad077b6af8d701b
Moritz Bischof <moritz.bischof@aleph-alpha.de>
2020-07-10 16:40:48 +0200

    rework broken Client::verify

--- src/client.rs
+++ src/client.rs
@@ -1,19 +1,20 @@
-use hyper::{client::{Client as HyperClient, HttpConnector}};
-#[cfg(feature = "with-rustls")]
-use hyper_rustls::HttpsConnector;
+use bytes::buf::ext::BufExt;
+use hyper::client::{Client as HyperClient, HttpConnector};
+
 #[cfg(feature = "with-openssl")]
 use hyper_openssl::HttpsConnector;
-use serde;
-use serde_json;
-use bytes::buf::ext::BufExt;
+#[cfg(feature = "with-rustls")]
+use hyper_rustls::HttpsConnector;
 
-use std::collections::BTreeMap;
-use std::time::{Duration, Instant};
-use std::ops::{Bound, Bound::{Included, Unbounded}};
-use std::collections::btree_map::Range;
-
-use crate::error::Error;
+use crate::cache_control::CacheControl;
+use crate::certs::CachedCerts;
+use crate::certs::Cert;
+use crate::Error;
 use crate::token::IdInfo;
+
+use jsonwebtoken::{Algorithm, DecodingKey, Validation};
+use serde::de::DeserializeOwned;
+use std::time::{Duration, Instant};
 
 pub struct Client {
     client: HyperClient<HttpsConnector<HttpConnector>>,
@@ -21,111 +22,63 @@ pub struct Client {
     pub hosted_domains: Vec<String>,
 }
 
-#[derive(Debug, Clone, Deserialize)]
-struct CertsObject {
-    keys: Vec<Cert>,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-struct Cert {
-    kid: String,
-    e: String,
-    kty: String,
-    alg: String,
-    n: String,
-    r#use: String,
-}
-
-type Key = String;
-
-#[derive(Clone)]
-pub struct CachedCerts {
-    keys: BTreeMap<Key, Cert>,
-    pub expiry: Option<Instant>,
-}
-
-impl CachedCerts {
-    pub fn new() -> Self {
-        Self {
-            keys: BTreeMap::new(),
-            expiry: None,
-        }
-    }
-
-    fn certs_url() -> &'static str {
-        "https://www.googleapis.com/oauth2/v2/certs"
-    }
-
-    fn get_range<'a>(&'a self, kid: &Option<String>) -> Result<Range<'a, Key, Cert>, Error> {
-        match kid {
-            None => Ok(self.keys.range::<String, (Bound<&String>, Bound<&String>)>((Unbounded, Unbounded))),
-            Some(kid) => {
-                if !self.keys.contains_key(kid) {
-                    return Err(Error::InvalidKey);
-                }
-                Ok(self.keys.range::<String, (Bound<&String>, Bound<&String>)>((Included(kid), Included(kid))))
-            }
-        }
-    }
-
-    /// Downloads the public Google certificates if it didn't do so already, or based on expiry of
-    /// their Cache-Control. Returns `true` if the certificates were updated.
-    pub async fn refresh_if_needed(&mut self) -> Result<bool, Error> {
-        let check = match self.expiry {
-            None => true,
-            Some(expiry) => expiry <= Instant::now(),
-        };
-
-        if !check {
-            return Ok(false);
-        }
-
-        let client = Client::new();
-        let certs : CertsObject = client.get_any(Self::certs_url(), &mut self.expiry).await?;
-        self.keys = BTreeMap::new();
-
-        for cert in certs.keys {
-            self.keys.insert(cert.kid.clone(), cert);
-        }
-
-        Ok(true)
-    }
-}
-
-impl Client {
-    pub fn new() -> Client {
+impl Default for Client {
+    fn default() -> Self {
         #[cfg(feature = "with-rustls")]
         let ssl = HttpsConnector::new();
         #[cfg(feature = "with-openssl")]
         let ssl = HttpsConnector::new().expect("unable to build HttpsConnector");
-        let client = HyperClient::builder().http1_max_buf_size(0x2000).keep_alive(false).build(ssl);
-        Client { client, audiences: vec![], hosted_domains: vec![] }
-    }
 
+        let client = HyperClient::builder()
+            .http1_max_buf_size(0x2000)
+            .pool_max_idle_per_host(0)
+            .build(ssl);
+
+        Client {
+            client,
+            audiences: vec![],
+            hosted_domains: vec![],
+        }
+    }
+}
+
+impl Client {
     /// Verifies that the token is signed by Google's OAuth cerificate,
     /// and check that it has a valid issuer, audience, and hosted domain.
-    ///
     /// Returns an error if the client has no configured audiences.
-    pub async fn verify(&self, id_token: &str, cached_certs: &CachedCerts) -> Result<IdInfo, Error> {
+    pub async fn verify(
+        &self,
+        id_token: &str,
+        cached_certs: &CachedCerts,
+    ) -> Result<IdInfo, Error> {
         let unverified_header = jsonwebtoken::decode_header(&id_token)?;
 
-        use jsonwebtoken::{Algorithm, Validation, DecodingKey};
-
-        for (_, cert) in cached_certs.get_range(&unverified_header.kid)? {
-            // Check each certificate
-
-            let mut validation = Validation::new(Algorithm::RS256);
-            validation.set_audience(&self.audiences);
-            let token_data = jsonwebtoken::decode::<IdInfo>(&id_token,
-                &DecodingKey::from_rsa_components(&cert.n, &cert.e),
-                &validation)?;
-
-            token_data.claims.verify(self)?;
-
-            return Ok(token_data.claims);
+        match unverified_header.kid {
+            Some(kid) => {
+                let cert = cached_certs.keys.get(&kid).ok_or(Error::InvalidKey)?;
+                self.verify_single(id_token, cert)
+            }
+            None => cached_certs
+                .keys
+                .values()
+                .flat_map(|cert| self.verify_single(id_token, cert))
+                .next()
+                .ok_or(Error::InvalidToken),
         }
+    }
 
-        Err(Error::InvalidToken)
+    fn verify_single(&self, id_token: &str, cert: &Cert) -> Result<IdInfo, Error> {
+        let mut validation = Validation::new(Algorithm::RS256);
+        validation.set_audience(&self.audiences);
+        let token_data = jsonwebtoken::decode::<IdInfo>(
+            &id_token,
+            &DecodingKey::from_rsa_components(&cert.n, &cert.e),
+            &validation,
+        )?;
+
+        token_data.claims.verify(self)?;
+
+        Ok(token_data.claims)
     }
 
     /// Checks the token using Google's slow OAuth-like authentication flow.
@@ -136,30 +89,38 @@ impl Client {
     /// This is NOT the recommended way to use the library, but can be used in combination with
     /// [IdInfo.verify](https://docs.rs/google-signin/latest/google_signin/struct.IdInfo.html#impl)
     /// for applications with more complex error-handling requirements.
-    pub async fn get_slow_unverified(&self, id_token: &str) -> Result<IdInfo<String, String>, Error> {
-        self.get_any(&format!("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token={}", id_token), &mut None).await
+    pub async fn get_slow_unverified(
+        &self,
+        id_token: &str,
+    ) -> Result<IdInfo<String, String>, Error> {
+        self.get_any(
+            &format!(
+                "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token={}",
+                id_token
+            ),
+            &mut None,
+        )
+        .await
     }
 
-    async fn get_any<T: serde::de::DeserializeOwned>(&self, url: &str, cache: &mut Option<Instant>) -> Result<T, Error> {
+    pub(crate) async fn get_any<T: DeserializeOwned>(
+        &self,
+        url: &str,
+        cache: &mut Option<Instant>,
+    ) -> Result<T, Error> {
         let url = url.parse().unwrap();
         let response = self.client.get(url).await.unwrap();
 
-        let status = response.status().as_u16();
-        match status {
-            200..=299 => {}
-            _ => {
-                return Err(Error::InvalidToken);
-            }
+        if !response.status().is_success() {
+            return Err(Error::InvalidToken);
         }
 
         if let Some(value) = response.headers().get("Cache-Control") {
             if let Ok(value) = value.to_str() {
-                if let Some(cc) = cache_control::CacheControl::from_value(value) {
+                if let Some(cc) = CacheControl::from_value(value) {
                     if let Some(max_age) = cc.max_age {
-                        let seconds = max_age.num_seconds();
-                        if seconds >= 0 {
-                            *cache = Some(Instant::now() + Duration::from_secs(seconds as u64));
-                        }
+                        let seconds = max_age.as_secs();
+                        *cache = Some(Instant::now() + Duration::from_secs(seconds as u64));
                     }
                 }
             }
